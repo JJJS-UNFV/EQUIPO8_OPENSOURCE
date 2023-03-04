b8d6062756534444629f4e298fd350b4f4c3fd44
JJJS-UNFV <126985630+JJJS-UNFV@users.noreply.github.com>
2023-03-04 15:15:42 -0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:social_smart_biker/src/login.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: LoginPage(),
+  ));
+}
